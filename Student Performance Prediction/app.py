43a757cdae536f13a932b5b226793998a8f3927f
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-04-16 20:43:58 +0530

    port changed to default

--- Student Performance Prediction/app.py
+++ Student Performance Prediction/app.py
@@ -57,5 +57,5 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
